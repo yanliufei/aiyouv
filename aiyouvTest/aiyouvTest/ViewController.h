89a2096f0fb8da0a2dc98935c92902705ef5459a
yanliufei <1332648043@qq.com>
2016-04-05 12:07:13 +0800

    初始化工程

---
+++ aiyouvTest/aiyouvTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aiyouvTest
+//
+//  Created by 颜刘飞 on 16/4/5.
+//  Copyright © 2016年 颜刘飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aiyouvTest/aiyouvTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aiyouvTest
+//
+//  Created by 颜刘飞 on 16/4/5.
+//  Copyright © 2016年 颜刘飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
